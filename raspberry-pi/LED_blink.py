bb91f81d9c4c63855cd94868d404e0f29bed10bb
honklin <hazeljconklin@gmail.com>
2023-09-06 10:47:58 -0400

    Launch 1

--- raspberry-pi/LED_blink.py
+++ raspberry-pi/LED_blink.py
@@ -3,7 +3,7 @@ import board
 import digitalio
 import time
 
-led = digitalio.DigitalInOut(board.GP1)
+led.digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
 
 while True:
---
+++ raspberry-pi/Launch1.py
@@ -0,0 +1,6 @@
+#type: ignore
+import time
+
+for (int i = 10; i >= 0; i = i - 1):
+    print(i)
+    time.delay(1)
