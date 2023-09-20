64fcc623c550a8f19ab06725ef9e2ecad46c9b2c
honklin <hazeljconklin@gmail.com>
2023-09-20 09:16:17 -0400

    crash 1

--- raspberry-pi/Crash1.py
+++ raspberry-pi/Crash1.py
@@ -1,15 +1,12 @@
 #type: ignore
 import time #delay library
-import board #board library
-import adafruit_mpu6050 #accelerometer library
+import board #pins library
+import adafruit_mpu6050 #accelerometer libary
 import busio
 
-sda = board.G14 #SDA pin
-scl = board.G15 #SCL pin
-i2c = busio.I2C(scl,sda) #I2C device declaration
-
-mpu = adafruit_mpu6050.MPU6050(i2c) #accelerometer
+i2c = busio.I2C(board.GP15, board.GP14) #I2C device declaration
+mpu = adafruit_mpu6050.MPU6050(i2c) #accelerometer declaration
 
 while True:
-    print(mpu.acceleration) #accelerometer output
+    print(mpu.acceleration) #accerelometer data
     time.sleep(.5)
