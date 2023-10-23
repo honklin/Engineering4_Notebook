885baf8f48cec78272b836540b2f4c22fe6afd9a
honklin <hazeljconklin@gmail.com>
2023-10-23 10:49:03 -0400

    landing 1

--- raspberry-pi/Landing1.py
+++ raspberry-pi/Landing1.py
@@ -10,17 +10,28 @@ def area(x,y,z):
     x = x.split(",")
     y = y.split(",")
     z = z.split(",")
-    a = (1/2)abs(x1(y2-y3)+x2(y3-y1)+x3(y1-y2))
+    x[0] = float(x[0])
+    x[1] = float(x[1])
+    y[0] = float(y[0])
+    y[1] = float(y[1])
+    z[0] = float(z[0])
+    z[1] = float(z[1])
+    a = x[0]*(y[1]-z[1])+y[0]*(z[1]-x[1])+z[0]*(x[1]-y[1])
+    a = (1/2) * abs(a)
     return a
 
 while True:
+    exit = True
     print("Enter the first coordinate x,y:")
-    x = float(input())
-    while(check(x)):
+    x = input()
+    while(check(x) and exit):
         print("Enter the second coordinate x,y:")
-        y = float(input())
-        while(check(y)):
+        y = input()
+        while(check(y) and exit):
             print("Enter the third coordinate x,y:")
-            z = float(input())
+            z = input()
             if(check(z)):
                 a = area(x,y,z)
+                print("Area: ")
+                print(a)
+                exit = False
