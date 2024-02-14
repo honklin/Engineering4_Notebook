6e541208b1c318195c3e158150c5c3f41ea84359
honklin <121810694+honklin@users.noreply.github.com>
2024-02-14 10:01:12 -0500

    Update test.py

--- code/test.py
+++ code/test.py
@@ -1,28 +1,31 @@
 #type: ignore
 import board
-<<<<<<< HEAD:raspberry-pi/test.py
 import time
-import digitalio
-import pwmio
-=======
+from adafruit_motor import motor
 import pwmio
 import digitalio
 import analogio
->>>>>>> f659b5a29b1f8d37ff19d9aa0d8226b6d0b6a333:code/test.py
 
-motor = pwmio.PWMOut(board.GP14,duty_cycle = 65535,frequency=5000)
+pwmA = pwmio.PWMOut(board.GP14, frequency=50)
+pwmB = pwmio.PWMOut(board.GP15, frequency=50)
+motor1 = motor.DCMotor(pwmA,pwmB)
 
-while (True):
-<<<<<<< HEAD:raspberry-pi/test.py
-    motor.duty_cycle = 4000
-    print(board.GP10) #digital
-    print(board.GP11) #analog
-    print(board.GP12) #I2C
-    print(board.GP13) #encoder
-=======
-    motor.duty_cycle = 40000
-    print(board.GP10) #digital
-    print(board.GP11) #analog
-    print(board.GP12) #I2C
-    print(board.GP13) #encoder
->>>>>>> f659b5a29b1f8d37ff19d9aa0d8226b6d0b6a333:code/test.py
+ch1 = digitalio.DigitalInOut(board.GP11)
+ch2 = digitalio.DigitalInOut(board.GP12)
+rot = 0
+old1 = ch1.value
+old2 = ch2.value
+
+while True:
+    motor1.throttle = .5
+
+    if ((ch1.value != old1) or (ch2.value != old2)):
+        rot += 1
+    print(rot/28)
+    old1 = ch1.value
+    old2 = ch2.value
+
+    #print("ch1:")
+    #print(ch1.value)
+    #print("ch2:")
+    #print(ch2.value)
